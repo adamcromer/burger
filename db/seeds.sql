28076b4749ca71b24b983a776182baaf7e3c12c1
adamjorycromer <adamjorycromer@gmail.com>
2019-02-16 23:40:02 -0700

    created config

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,4 +3,5 @@ USE burgers_db;
 INSERT INTO burgers (burger_name)
 VALUES ("Monty's Good Burger"),
  ("DC Vegetarian Bacon Double Cheeseburger"),
- ("The Beyond Burger")
+ ("The Beyond Burger"),
+ ("Hire's Veggie Burger")
